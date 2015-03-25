5f9608a4d36fd1cea771a1a6173933ebd5d7b894
magicdebater <magicdebater@gmail.com>
2015-03-25 11:00:44 +0800

    bugs

--- midexam/c14006117/start.cpp
+++ midexam/c14006117/start.cpp
@@ -26,7 +26,7 @@ int main()
 		x[i] = cos(M_PI*2*i/10); // 整數運算仍為整數,除非先用double [try]: cos(2*i/10*M_PI) 
 		printf("x[%d]=%f\n",i,x[i]); 
 	}
-	for (i=0;i<8)
+	for (i=0;i<8;i=i+1)
 	{
 		u[i]=(x[i]+x[i+1]+x[i+2])/3;
 	}
@@ -35,11 +35,10 @@ int main()
 			 [0 1 1 1 0 ... 0] x [1] 
 	*/
 	// 以上是baby problem 
-	youth_problem(20,3);
 	return 0;  
 }
 
-int youth_problem(int n, int k) //副程式 
+int youth_problem(int n, int K) //副程式 
 {
 	int i, j, k; // Z
 	double a, b, c; // R
@@ -56,7 +55,7 @@ int youth_problem(int n, int k) //副程式
 	   u[n-k] = ( x[n-k] + x[n-k+1] +... + x[n-1] )/k
 	*/
 
-	for (i=0;i<n-k,i=i+1)
+	for (i=0;i<n-K;i=i+1)
 	{
 		/*
 		u[i] = x[i] + x[i+1] + ... + x[i+k-1]
@@ -67,17 +66,21 @@ int youth_problem(int n, int k) //副程式
 		...
 		step j : u[i]=u[i] + x[i+j]
 		*/
-		u[i] = x[i] // step0
-		for (j=0;j=k-1;j=j+1)
+		u[i] = x[i]; // step0
+		for (j=0;j=K-1;j=j+1)
 		{
 			u[i]=u[i]+x[i+j];
 		}
-		u[i] = u[i]/k;
+		u[i] = u[i]/K;
 	}
 }
 
-int university_problem(int n, int k)//簡化加法,乘法
+int university_problem(int n, int K)//簡化加法
 {
+	int i, j, k; // Z
+	double a, b, c; // R
+	double x[n], u[n]; //vector
+	double A[n][n]; 
 	// Moving Average, k個平均
 	/* u[0] = ( x[0] + x[1] + ... + x[k-1] )/k
 	   u[1] = ( x[1] + x[2] + ... + x[k] )/k
@@ -102,15 +105,16 @@ int university_problem(int n, int k)//簡化加法,乘法
 	i = 0;
 	u[i] = x[i]; // step 0
 	
-	for (j=1,j=k,j=j+1) // j = 1 2 ... k-1 
+	for (j=1;j=K;j=j+1) // j = 1 2 ... k-1 
 	{
 		u[i] = u[i] + x[i+j];
 	}
-	u[i] = u[i]/k;
-	for (i=1,i<=n-k,i=i+1)
+	u[i] = u[i]/K;
+	for (i=1;i<=n-K;i=i+1)
 	{
-		u[i] = u[i-1] + ( x[k+i-1]-x[i-1] )/k;
+		u[i] = u[i-1] + ( x[k+i-1]-x[i-1] )/K;
 	} 
+	return 0;
 } 
  
 
